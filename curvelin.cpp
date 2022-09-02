d9af7487e07babb8ab40aa02344090b780dc52ca
Krishnakumar P <72070696+krishnakpt18@users.noreply.github.com>
2022-09-02 07:50:34 +0530

    Add files via upload

--- curvelin.cpp
+++ curvelin.cpp
@@ -5,34 +5,46 @@
 using namespace std;
 int main(){
 	int i,j,k,n;
-	double x[n]={},xd[n]={},yd[n]={},y[n]={},x2[n]={},xy[n]={},addx=0,addy=0,addx2=0,addxy=0;
+	double x[n]={},xd[n]={},yd[n]={},y[n]={},x2[n]={},xy[n]={},addx=0,addy=0,addx2=0,addxy=0,a,b;
 	ifstream fin("in.txt");
-	
+
 	ofstream fans("ans.txt");
 	if (fin.is_open()) {
 		fin>>n;
 	}
-	
-	
+
+
 	for (i = 0; i < n; ++i) {
 		fin>>x[i];
-		cout<<x[i];
+
 	}	cout<<"\n";
-	
+
 	for (i = n; i < n+n; ++i) {
 		fin>>y[i];
-		cout<<y[i];
+
 	}	cout<<"\n";
-		
+
+	for(i=0;i<n;++i){
+        addx+=x[i];
+        addy+=y[i];
+        addx2+=x2[i];
+        addxy+=xy[i];
+	}
 	for (i = 0; i < n; ++i) {
 		x2[i]=x[i]*x[i];
-		cout<<x2[i];
-	}	cout<<"\n";
-	for (i = 0; i < n; ++i) {
 		xy[i]=x[i]*y[i];
-		cout<<xy[i];
+
 	}	cout<<"\n";
-		
+
+	for (i = 0; i < n; ++i) {
+
+		cout<<x[i]<<setw(16)<<y[i]<<setw(8)<<x2[i]<<setw(10)<<xy[i];
+
+	}	cout<<"\n";
+    a=(n*addxy-addx*addy)/(n*addx2-(addx*addx));
+    b=(addy*addx2-addx*addxy)/(n*addx2-(addx*addx));
+    cout<<endl;
+    cout<<a<<"     "<<b;
 
 
 
@@ -43,5 +55,5 @@ int main(){
 
 
 }
-	
+
 
